46d5751fb53d32cfa3524bb74df12751136839a2
SongulKurgan <songulkurgan36@gmail.com>
2022-06-22 11:27:38 +0300

    .

---
+++ Sas.Restaurant.UI.BackOffice/Garson/FrmGarson.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Sas.Restaurant.UI.BackOffice.Garson
+{
+    partial class FrmGarson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.controlMenu = new Sas.Restaurant.UserControls.ControlAnaMenuAlt();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.gridControlGarson = new DevExpress.XtraGrid.GridControl();
+            this.gridGarson = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colAdi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSoyadi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAciklama = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlGarson)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridGarson)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.controlMenu);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupControl1.Location = new System.Drawing.Point(0, 617);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(1185, 100);
+            this.groupControl1.TabIndex = 7;
+            this.groupControl1.Text = "Menü";
+            // 
+            // controlMenu
+            // 
+            this.controlMenu.ButtonFont = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.controlMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlMenu.Location = new System.Drawing.Point(2, 28);
+            this.controlMenu.Name = "controlMenu";
+            this.controlMenu.Size = new System.Drawing.Size(1181, 70);
+            this.controlMenu.TabIndex = 0;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.labelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelControl1.Location = new System.Drawing.Point(0, 0);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(1185, 69);
+            this.labelControl1.TabIndex = 6;
+            this.labelControl1.Text = "Garsonlar";
+            // 
+            // gridControlGarson
+            // 
+            this.gridControlGarson.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlGarson.Location = new System.Drawing.Point(0, 69);
+            this.gridControlGarson.MainView = this.gridGarson;
+            this.gridControlGarson.Name = "gridControlGarson";
+            this.gridControlGarson.Size = new System.Drawing.Size(1185, 548);
+            this.gridControlGarson.TabIndex = 8;
+            this.gridControlGarson.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridGarson});
+            // 
+            // gridGarson
+            // 
+            this.gridGarson.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colAdi,
+            this.colSoyadi,
+            this.colAciklama});
+            this.gridGarson.GridControl = this.gridControlGarson;
+            this.gridGarson.Name = "gridGarson";
+            // 
+            // colAdi
+            // 
+            this.colAdi.Caption = "Adı";
+            this.colAdi.FieldName = "Adi";
+            this.colAdi.MinWidth = 25;
+            this.colAdi.Name = "colAdi";
+            this.colAdi.Visible = true;
+            this.colAdi.VisibleIndex = 0;
+            this.colAdi.Width = 94;
+            // 
+            // colSoyadi
+            // 
+            this.colSoyadi.Caption = "Soyadı";
+            this.colSoyadi.FieldName = "Soyadi";
+            this.colSoyadi.MinWidth = 25;
+            this.colSoyadi.Name = "colSoyadi";
+            this.colSoyadi.Visible = true;
+            this.colSoyadi.VisibleIndex = 1;
+            this.colSoyadi.Width = 94;
+            // 
+            // colAciklama
+            // 
+            this.colAciklama.Caption = "Açıklama";
+            this.colAciklama.FieldName = "Aciklama";
+            this.colAciklama.MinWidth = 25;
+            this.colAciklama.Name = "colAciklama";
+            this.colAciklama.Visible = true;
+            this.colAciklama.VisibleIndex = 2;
+            this.colAciklama.Width = 94;
+            // 
+            // FrmGarson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 717);
+            this.Controls.Add(this.gridControlGarson);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.labelControl1);
+            this.Name = "FrmGarson";
+            this.Text = "FrmGarson";
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlGarson)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridGarson)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private UserControls.ControlAnaMenuAlt controlMenu;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraGrid.GridControl gridControlGarson;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridGarson;
+        private DevExpress.XtraGrid.Columns.GridColumn colAdi;
+        private DevExpress.XtraGrid.Columns.GridColumn colSoyadi;
+        private DevExpress.XtraGrid.Columns.GridColumn colAciklama;
+    }
+}
---
+++ Sas.Restaurant.UI.BackOffice/Garson/FrmGarson.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.XtraEditors;
+using Sas.Restaurant.Business.Workers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sas.Restaurant.UI.BackOffice.Garson
+{
+    public partial class FrmGarson : DevExpress.XtraEditors.XtraForm
+    {
+        RestaurantWorker worker = new RestaurantWorker();
+        public FrmGarson()
+        {
+            InitializeComponent();
+            Listele();
+        }
+        void Listele()
+        {
+            worker.GarsonService.Load(null);
+            gridControlGarson.DataSource = worker.GarsonService.BindingList();
+        }
+    }
+}
